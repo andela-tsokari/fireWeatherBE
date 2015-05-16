d899b12f72ee3afc8dab0a195379221a349b408f
Tams Sokari <tams.sokari@andela.co>
2015-05-16 01:46:01 +0100

    set up routes

--- app/routes/fire-weather-be.route.js
+++ app/routes/fire-weather-be.route.js
@@ -4,35 +4,11 @@ module.exports = function(router) {
   'use strict';
 
   router
-    .route('/lagos')
+    .route('/:place')
     .get(weather.getToday);
 
   router
-    .route('/lagos/five')
-    .get(weather.getFiveDays);
-
-  router
-    .route('/new-york')
-    .get(weather.getToday);
-
-  router
-    .route('/new-york/five')
-    .get(weather.getFiveDays);
-
-  router
-    .route('/san-francisco')
-    .get(weather.getToday);
-
-  router
-    .route('/san-francisco/five')
-    .get(weather.getFiveDays);
-
-  router
-    .route('/london')
-    .get(weather.getToday);
-
-  router
-    .route('/london/five')
+    .route('/:place/five')
     .get(weather.getFiveDays);
 
 };
