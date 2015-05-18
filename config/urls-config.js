f9e6ab093dc745763bcfb2f880748239add211ea
Tams Sokari <tams.sokari@andela.co>
2015-05-18 16:55:32 +0100

    configure urls

--- config/urls-config.js
+++ config/urls-config.js
@@ -1,7 +1,9 @@
+var firebase = require('firebase');
+
 var now = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-var forecasts = 'http://api.openweathermap.org/data/2.5/forecast/daily/?units=metric&cnt=5&q=';
+var forecasts = 'http://api.openweathermap.org/data/2.5/forecast/daily/?units=metric&cnt=6&q=';
 
-var rootRef = 'https://fireweather.firebaseio.com/';
+var rootRef = new firebase('https://fireweather.firebaseio.com/');
 
 module.exports = [now, forecasts, rootRef];
